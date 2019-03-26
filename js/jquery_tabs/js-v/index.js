d706193942dbd3d887c2c2587d3ce936849d32a7
Erzhuyijian <1821344102@qq.com>
2019-03-26 23:52:59 +0800

    add jquery tabs vs. js tabs

---
+++ js/jquery_tabs/js-v/index.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.mod-tab .tab').forEach(function(node){
+  node.addEventListener('click', function(){
+    var index;
+    this.parentElement.querySelectorAll('.tab').forEach(function(tab,idx){
+      tab.classList.remove('active');
+      if(node === tab) {
+        console.log(idx);
+        index = idx;
+      }
+    });
+    this.classList.add('active');
+    this.parentElement.nextElementSibling.querySelectorAll('.panel').forEach(function(panel){
+      panel.classList.remove('active');
+    })
+    this.parentElement.nextElementSibling.querySelectorAll('.panel')[index].classList.add('active');
+  });
+});
